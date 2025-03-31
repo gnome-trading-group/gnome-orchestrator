ac5320681950de917045b9546971c7986a196e19
mprey <masonprey7@gmail.com>
2025-03-31 14:18:41 -0400

    fix(aggregator): set instanceClass

--- src/main/java/group/gnometrading/collectors/AggregatorOrchestrator.java
+++ src/main/java/group/gnometrading/collectors/AggregatorOrchestrator.java
@@ -5,10 +5,13 @@ import group.gnometrading.di.OrchestratorLambda;
 
 public class AggregatorOrchestrator extends OrchestratorLambda<Void, Void> {
 
+    static {
+        instanceClass = AggregatorOrchestrator.class;
+    }
+
     @Override
     protected Void execute(Void input, Context context) {
         System.out.println("The lambda has been executed!");
         return null;
     }
-    
 }
