c413b5ee769ae27067d6338985ebe1ee3280ccac
mprey <masonprey7@gmail.com>
2025-04-16 11:15:23 -0400

    feat(collectors): allow multiple listings on the same orchestrator

--- src/main/java/group/gnometrading/collectors/DefaultCollectorOrchestrator.java
+++ src/main/java/group/gnometrading/collectors/DefaultCollectorOrchestrator.java
@@ -7,21 +7,32 @@ import group.gnometrading.di.Named;
 import group.gnometrading.di.Orchestrator;
 import group.gnometrading.di.Provides;
 import group.gnometrading.di.Singleton;
+import group.gnometrading.gateways.MarketInboundGateway;
 import group.gnometrading.ipc.IPCManager;
 import group.gnometrading.resources.Properties;
 import group.gnometrading.schemas.SchemaType;
 import group.gnometrading.shared.AWSModule;
 import group.gnometrading.sm.Listing;
+import group.gnometrading.utils.AgentUtils;
 import io.aeron.Aeron;
+import io.aeron.Publication;
+import io.aeron.Subscription;
 import io.aeron.driver.MediaDriver;
+import org.agrona.ErrorHandler;
+import org.agrona.concurrent.AgentRunner;
+import org.agrona.concurrent.YieldingIdleStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.services.s3.S3Client;
 
 import java.io.IOException;
 import java.time.Clock;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class DefaultCollectorOrchestrator extends Orchestrator implements AWSModule {
 
-    protected static final String STREAM_NAME = "collector";
+    private static final Logger logger = LoggerFactory.getLogger(DefaultCollectorOrchestrator.class);
 
     @Provides
     @Named("PROPERTIES_PATH")
@@ -70,29 +81,78 @@ public abstract class DefaultCollectorOrchestrator extends Orchestrator implemen
     }
 
     @Provides
-    public Listing provideListing(Properties properties, SecurityMaster securityMaster) {
-        int listingId = Integer.parseInt(System.getenv("LISTING_ID"));
-        return securityMaster.getListing(listingId);
+    @Singleton
+    @Named("LISTINGS")
+    public List<Listing> provideListings(SecurityMaster securityMaster) {
+        List<Listing> output = new ArrayList<>();
+        String listingIds = System.getenv("LISTING_IDS");
+        for (String listingId : listingIds.split(",")) {
+            output.add(securityMaster.getListing(Integer.parseInt(listingId)));
+        }
+        return output;
     }
 
-    @Provides
-    @Singleton
-    public BulkMarketDataCollector provideBulkMarketDataCollector(
-            Clock clock,
-            IPCManager ipcManager,
-            S3Client s3Client,
-            Listing listing,
-            @Named("BUCKET_NAME") String bucketName,
-            SchemaType schemaType
+    private BulkMarketDataCollector createBulkMarketDataCollector(
+            Subscription subscription,
+            Listing listing
     ) {
         return new BulkMarketDataCollector(
-                clock,
-                ipcManager,
-                STREAM_NAME,
-                s3Client,
+                subscription,
+                getInstance(Clock.class),
+                getInstance(S3Client.class),
                 listing,
-                bucketName,
-                schemaType
+                getInstance(String.class, "BUCKET_NAME"),
+                defaultSchemaType()
         );
+    }
+
+    protected abstract MarketInboundGateway createInboundGateway(
+        Publication publication,
+        Listing listing
+    );
+
+    protected abstract SchemaType defaultSchemaType();
+
+    private ErrorHandler handleInboundError(MarketInboundGateway gateway) {
+        return (error) -> {
+            logger.info("Unknown error occurred in market inbound gateway", error);
+            logger.info("Marking the inbound gateway for reconnection...");
+            gateway.markReconnect();
+        };
+    }
+
+    private void handleOutboundError(Throwable error) {
+        logger.info("Unknown error occurred in market update collector", error);
+        // TODO: What should we do here?
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected void configure() {
+        logger.info("Beginning collector for: {}", this.getClass().getSimpleName());
+        List<Listing> listings = this.getInstance(List.class, "LISTINGS");
+        IPCManager ipcManager = getInstance(IPCManager.class);
+
+        for (Listing listing : listings) {
+            String streamName = "collector#" + listing.listingId();
+
+            MarketInboundGateway marketInboundGateway = createInboundGateway(ipcManager.addExclusivePublication(streamName), listing);
+            BulkMarketDataCollector bulkMarketDataCollector = createBulkMarketDataCollector(ipcManager.addSubscription(streamName), listing);
+
+            final var publicationAgentRunner = new AgentRunner(new YieldingIdleStrategy(), this.handleInboundError(marketInboundGateway), null, marketInboundGateway);
+            final var subscriptionAgentRunner = new AgentRunner(new YieldingIdleStrategy(), this::handleOutboundError, null, bulkMarketDataCollector);
+
+            AgentUtils.startRunnerWithShutdownProtection(publicationAgentRunner);
+            AgentUtils.startRunnerWithShutdownProtection(subscriptionAgentRunner);
+
+            logger.info("Started listing {} on exchange {} with schema {} on class {}",
+                    listing.exchangeSecuritySymbol(),
+                    listing.exchangeId(),
+                    defaultSchemaType(),
+                    marketInboundGateway.getClass().getSimpleName()
+            );
+        }
+
+        logger.info("Finished configuring collector");
     }
 }
--- src/main/java/group/gnometrading/collectors/HyperliquidCollectorOrchestrator.java
+++ src/main/java/group/gnometrading/collectors/HyperliquidCollectorOrchestrator.java
@@ -2,26 +2,18 @@ package group.gnometrading.collectors;
 
 import group.gnometrading.codecs.json.JSONDecoder;
 import group.gnometrading.codecs.json.JSONEncoder;
-import group.gnometrading.collector.BulkMarketDataCollector;
 import group.gnometrading.di.Provides;
-import group.gnometrading.di.Singleton;
 import group.gnometrading.gateways.JSONWebSocketMarketInboundGateway;
 import group.gnometrading.gateways.MarketInboundGateway;
 import group.gnometrading.gateways.exchanges.hyperliquid.HyperliquidInboundGateway;
-import group.gnometrading.ipc.IPCManager;
 import group.gnometrading.networking.sockets.factory.NativeSSLSocketFactory;
 import group.gnometrading.networking.websockets.WebSocketClient;
 import group.gnometrading.networking.websockets.WebSocketClientBuilder;
 import group.gnometrading.resources.Properties;
 import group.gnometrading.schemas.SchemaType;
 import group.gnometrading.sm.Listing;
-import group.gnometrading.utils.AgentUtils;
 import io.aeron.Publication;
-import org.agrona.concurrent.AgentRunner;
 import org.agrona.concurrent.SystemEpochNanoClock;
-import org.agrona.concurrent.YieldingIdleStrategy;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
@@ -33,15 +25,12 @@ public class HyperliquidCollectorOrchestrator extends DefaultCollectorOrchestrat
         instanceClass = HyperliquidCollectorOrchestrator.class;
     }
 
-    private static final Logger logger = LoggerFactory.getLogger(HyperliquidCollectorOrchestrator.class);
-
     @Provides
     public URI provideURI(Properties properties) throws URISyntaxException {
         return new URI(properties.getStringProperty("hyperliquid.ws.url"));
     }
 
-    @Singleton
-    @Provides
+    @Provides // Do not make this a singleton...unless you want problems, son.
     public WebSocketClient provideWSClient(URI uri) throws IOException {
         return new WebSocketClientBuilder()
                 .withURI(uri)
@@ -50,24 +39,13 @@ public class HyperliquidCollectorOrchestrator extends DefaultCollectorOrchestrat
                 .build();
     }
 
-    @Provides
-    @Singleton
-    public Publication providePublication(IPCManager ipcManager) {
-        return ipcManager.addExclusivePublication(STREAM_NAME);
-    }
-
-    @Singleton
-    @Provides
-    public MarketInboundGateway provideMarketInboundGateway(
-            WebSocketClient webSocketClient,
-            Publication publication,
-            SchemaType schemaType,
-            Listing listing
-    ) {
+    @Override
+    protected MarketInboundGateway createInboundGateway(Publication publication, Listing listing) {
+        WebSocketClient webSocketClient = getInstance(WebSocketClient.class);
         return new HyperliquidInboundGateway(
                 publication,
                 new SystemEpochNanoClock(),
-                schemaType,
+                defaultSchemaType(),
                 webSocketClient,
                 new JSONDecoder(),
                 new JSONEncoder(),
@@ -76,37 +54,8 @@ public class HyperliquidCollectorOrchestrator extends DefaultCollectorOrchestrat
         );
     }
 
-    @Provides
-    public SchemaType provideSchemaType() {
-        return SchemaType.MBP_10;
-    }
-
-    public void handleInboundError(Throwable error) {
-        logger.info("Unknown error occurred in market inbound gateway", error);
-
-        logger.info("Marking the inbound gateway for reconnection...");
-        MarketInboundGateway marketInboundGateway = getInstance(MarketInboundGateway.class);
-        marketInboundGateway.markReconnect();
-    }
-
-    public void handleOutboundError(Throwable error) {
-        logger.info("Unknown error occurred in market update collector", error);
-        // TODO: What should we do here?
-    }
-
     @Override
-    protected void configure() {
-        MarketInboundGateway marketInboundGateway = getInstance(MarketInboundGateway.class);
-        BulkMarketDataCollector marketUpdateCollector = getInstance(BulkMarketDataCollector.class);
-        Listing listing = getInstance(Listing.class);
-        SchemaType schemaType = getInstance(SchemaType.class);
-
-        final var publicationAgentRunner = new AgentRunner(new YieldingIdleStrategy(), this::handleInboundError, null, marketInboundGateway);
-        final var subscriptionAgentRunner = new AgentRunner(new YieldingIdleStrategy(), this::handleOutboundError, null, marketUpdateCollector);
-
-        AgentUtils.startRunnerWithShutdownProtection(publicationAgentRunner);
-        AgentUtils.startRunnerWithShutdownProtection(subscriptionAgentRunner);
-
-        logger.info("Started everything up with listing {} on exchange {} with schema {}", listing.exchangeSecuritySymbol(), listing.exchangeId(), schemaType);
+    protected SchemaType defaultSchemaType() {
+        return SchemaType.MBP_10;
     }
 }
