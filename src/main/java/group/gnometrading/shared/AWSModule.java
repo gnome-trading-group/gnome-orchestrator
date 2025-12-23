5bc26c35926a3cecc9cfde16f9198c351ce912c9
mprey <masonprey7@gmail.com>
2025-12-23 14:13:53 -0500

    fix(aws): supply region to bucket

--- src/main/java/group/gnometrading/shared/AWSModule.java
+++ src/main/java/group/gnometrading/shared/AWSModule.java
@@ -4,6 +4,7 @@ import group.gnometrading.di.Named;
 import group.gnometrading.di.Provides;
 import group.gnometrading.di.Singleton;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -16,9 +17,18 @@ public interface AWSModule {
     }
 
     @Provides
+    default Region provideAWSRegion() {
+        return Region.of(System.getenv().getOrDefault("AWS_REGION", "us-east-1"));
+    }
+
+    @Provides
     @Singleton
-    default S3Client provideS3Client(@Named("AWS_PROFILE") String awsProfile) {
-        var builder = S3Client.builder();
+    default S3Client provideS3Client(
+            @Named("AWS_PROFILE") String awsProfile, Region awsRegion
+    ) {
+        var builder = S3Client.builder()
+                .region(awsRegion)
+                .crossRegionAccessEnabled(true);
         if (awsProfile != null && !awsProfile.isEmpty()) {
             builder.credentialsProvider(ProfileCredentialsProvider.create(awsProfile));
         }
@@ -27,8 +37,11 @@ public interface AWSModule {
 
     @Provides
     @Singleton
-    default DynamoDbClient provideDynamoDbClient(@Named("AWS_PROFILE") String awsProfile) {
-        var builder = DynamoDbClient.builder();
+    default DynamoDbClient provideDynamoDbClient(
+            @Named("AWS_PROFILE") String awsProfile,
+            Region awsRegion
+    ) {
+        var builder = DynamoDbClient.builder().region(awsRegion);
         if (awsProfile != null && !awsProfile.isEmpty()) {
             builder.credentialsProvider(ProfileCredentialsProvider.create(awsProfile));
         }
