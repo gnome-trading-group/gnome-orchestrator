7d9b13bdc99835dbb197ba8a5022c7e13b193878
mprey <masonprey7@gmail.com>
2026-08-07 14:00:10 -0400

    fix: hard-code key IDs

--- cdk/lib/config.ts
+++ cdk/lib/config.ts
@@ -8,15 +8,18 @@ export const ECS_REGIONS = ['us-east-1', 'ap-northeast-1'];
 export interface OrchestratorConfig {
   account: GnomeAccount;
   ecsRegions: string[];
+  registryApiKeyId: string;
 }
 
 export const CONFIGS: { [stage in Stage]?: OrchestratorConfig } = {
   [Stage.DEV]: {
     account: GnomeAccount.InfraDev,
     ecsRegions: ECS_REGIONS,
+    registryApiKeyId: 'rb0pbivke8',
   },
   [Stage.PROD]: {
     account: GnomeAccount.InfraProd,
     ecsRegions: ECS_REGIONS,
+    registryApiKeyId: 'mj0thnxe96',
   },
 };
